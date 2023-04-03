161553b48156228f0d550714844c4de1cde9a456
Wahyu Kurniawan <ayungavis@gmail.com>
2023-04-03 10:19:19 +0700

    🔧 chore: update eslint rules

--- packages/config/eslint/index.js
+++ packages/config/eslint/index.js
@@ -9,9 +9,18 @@ const config = {
     "turbo",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "eslint:recommended",
+    "plugin:react/recommended",
     "prettier",
   ],
   rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-sort-props": [
+      "error",
+      {
+        callbacksLast: true,
+      },
+    ],
     "@next/next/no-html-link-for-pages": "off",
     "@typescript-eslint/restrict-template-expressions": "off",
     "@typescript-eslint/no-unused-vars": [
@@ -25,6 +34,14 @@ const config = {
     "@typescript-eslint/consistent-type-imports": [
       "error",
       { prefer: "type-imports", fixStyle: "inline-type-imports" },
+    ],
+    "@typescript-eslint/no-misused-promises": [
+      2,
+      {
+        checksVoidReturn: {
+          attributes: false,
+        },
+      },
     ],
   },
   ignorePatterns: ["**/*.config.js", "**/*.config.cjs", "packages/config/**"],
