c880298379b269fdcd42de3579dd6623f62adabc
Wahyu Kurniawan <ayungavis@gmail.com>
2023-04-03 10:19:19 +0700

    🎨 style: update tailwind default config style

--- packages/config/tailwind/index.js
+++ packages/config/tailwind/index.js
@@ -21,17 +21,17 @@ const config = {
       white: "#ffffff",
       black: "#000000",
       primary: {
-        25: "#cbd4ff",
-        50: "#b9c6ff",
-        100: "#a8b8ff",
-        200: "#97a9ff",
-        300: "#859bff",
-        400: "#748dff",
-        500: "#627eff",
-        600: "#5170ff",
-        700: "#4965e6",
-        800: "#415acc",
-        900: "#394eb3",
+        25: "#fcfaff",
+        50: "#f9f5ff",
+        100: "#f4ebff",
+        200: "#e9d7fe",
+        300: "#d6bbfb",
+        400: "#b692f6",
+        500: "#9e77ed",
+        600: "#7f56d9",
+        700: "#6941c6",
+        800: "#53389e",
+        900: "#42307d",
       },
       error: {
         25: "#fffbfa",
@@ -401,6 +401,24 @@ const config = {
     extend: {
       fontFamily: {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      spacing: {
+        4.5: "1.125rem", // 18px
+      },
+      boxShadow: {
+        xs: "0px 1px 2px rgba(16, 24, 40, 0.05);",
+        sm: "0px 1px 3px rgba(16, 24, 40, 0.1), 0px 1px 2px rgba(16, 24, 40, 0.06);",
+        md: "0px 4px 8px -2px rgba(16, 24, 40, 0.1), 0px 2px 4px -2px rgba(16, 24, 40, 0.06);",
+        lg: "0px 12px 16px -4px rgba(16, 24, 40, 0.08), 0px 4px 6px -2px rgba(16, 24, 40, 0.03);",
+        xl: "0px 20px 24px -4px rgba(16, 24, 40, 0.08), 0px 8px 8px -4px rgba(16, 24, 40, 0.03);",
+        "2xl": "0px 24px 48px -12px rgba(16, 24, 40, 0.18);",
+        "3xl": "0px 32px 64px -12px rgba(16, 24, 40, 0.14);",
+        focus: "0px 1px 2px rgba(16, 24, 40, 0.05), 0px 0px 0px 4px #F4EBFF;",
+        "focus-secondary":
+          "0px 1px 2px rgba(16, 24, 40, 0.05), 0px 0px 0px 4px #F2F4F7;",
+        "focus-error":
+          "0px 1px 2px rgba(16, 24, 40, 0.05), 0px 0px 0px 4px #FEE4E2;",
+        "focus-checkbox": "0px 0px 0px 4px #F4EBFF",
       },
     },
   },
