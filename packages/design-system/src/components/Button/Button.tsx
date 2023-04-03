ded1c3aa558b47a3cf2e2d54879854d2a92b4829
Wahyu Kurniawan <ayungavis@gmail.com>
2023-04-03 10:19:19 +0700

    🐛 fix: ci error on packages

--- packages/design-system/src/components/Button/Button.tsx
+++ packages/design-system/src/components/Button/Button.tsx
@@ -1,4 +1,5 @@
 import { forwardRef, type ComponentPropsWithoutRef } from "react";
+
 import { cn } from "@myapp/utils";
 
 import { buttonVariants, type ButtonVariants } from "./Button.variants";
@@ -8,7 +9,7 @@ export type ButtonProps = ComponentPropsWithoutRef<"button"> & ButtonVariants;
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, children, ...props }, ref) => {
     return (
-      <button ref={ref} className={cn(buttonVariants(), className)} {...props}>
+      <button className={cn(buttonVariants(), className)} ref={ref} {...props}>
         {children}
       </button>
     );
