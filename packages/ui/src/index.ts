ed93b0087633dd55e885377decc5263d07a8240b
Wahyu Kurniawan <ayungavis@gmail.com>
2024-01-23 14:22:23 +0700

    🔧 chore: replace `cva` with `tailwind-variants`

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,9 +1,13 @@
-import type { CxOptions } from "class-variance-authority";
-import { cx } from "class-variance-authority";
+import type { ClassValue } from "clsx";
+import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-function cn(...inputs: CxOptions) {
-  return twMerge(cx(inputs));
+/**
+ * Returns a merged class name string by merging and processing multiple class names and Tailwind CSS styles.
+ *
+ * @param {...string[]} args - One or more class names and/or Tailwind CSS styles to be merged.
+ * @returns {string} - The merged class name string.
+ */
+export function cn(...args: ClassValue[]): string {
+  return twMerge(clsx(args));
 }
-
-export { cn };
--- packages/ui/src/label.tsx
+++ packages/ui/src/label.tsx
@@ -1,13 +1,15 @@
-import type { VariantProps } from "class-variance-authority";
+import type { VariantProps } from "tailwind-variants";
 import * as React from "react";
 import * as LabelPrimitive from "@radix-ui/react-label";
-import { cva } from "class-variance-authority";
+import { tv } from "tailwind-variants";
 
 import { cn } from ".";
 
-const labelVariants = cva(
-  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
-);
+const labelVariants = tv({
+  base: [
+    "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+  ],
+});
 
 const Label = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
