5cac537dc335751c682f42ebeb20b8051d3f6af4
Wahyu Kurniawan <ayungavis@gmail.com>
2024-01-23 14:05:19 +0700

    🔧 chore: change to vercel env

--- apps/auth-proxy/routes/[...auth].ts
+++ apps/auth-proxy/routes/[...auth].ts
@@ -2,12 +2,10 @@ import { Auth } from "@auth/core";
 import Discord from "@auth/core/providers/discord";
 import { eventHandler, toWebRequest } from "h3";
 
-const isProd = process.env.NODE_ENV === "production";
-
 export default eventHandler(async (event) =>
   Auth(toWebRequest(event), {
     secret: process.env.AUTH_SECRET,
-    trustHost: false,
+    trustHost: !!process.env.VERCEL,
     redirectProxyUrl: process.env.AUTH_REDIRECT_PROXY_URL,
     providers: [
       Discord({
